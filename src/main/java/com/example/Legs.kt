e5a0094a33b2da2d35bb06de3a6f0034275714bf
Gareth Park <garethpark@yahoo.com>
2024-09-29 09:23:41 +0100

    inline code

--- src/main/java/com/example/Legs.kt
+++ src/main/java/com/example/Legs.kt
@@ -12,14 +12,17 @@ object Legs {
         return Optional.ofNullable(longestLegOver(legs, duration))
     }
 
-    fun longestLegOver(legs: List<Leg>, duration: Duration): Leg? {
-        var result: Leg? = null
-        for (leg in legs) {
-            if (isLongerThan(leg, duration))
-                if (result == null ||
-                    isLongerThan(leg, result.plannedDuration)
-                )
-                    result = leg
+    fun longestLegOver(
+        legs: List<Leg>,
+        duration: Duration
+        ): Leg? {
+            var result: Leg? = null
+            for (leg in legs) {
+                if (isLongerThan(leg, duration))
+                    if (result == null ||
+                        isLongerThan(leg, result.plannedDuration)
+                    )
+                        result = leg
         }
         return result
     }
